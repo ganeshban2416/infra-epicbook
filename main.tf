15e9e366889f12522abfe954c5fd89111cabc1b6
ganeshban2416 <ganeshban163@gmail.com>
2026-04-16 18:08:40 +0530

    Added SSH key for VM

--- main.tf
+++ main.tf
@@ -2,6 +2,9 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+variable "resource_group_name" {
+  default = "epicbook-rg-01"
+}
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "epicbook-vnet"
@@ -15,4 +18,58 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+}
+
+
+
+# Public IP
+resource "azurerm_public_ip" "pip" {
+  name                = "epicbook-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "epicbook-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+# Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "epicbook-vm-gb"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D2ls_v5"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEuQCp+cYyIKCe3N4G2Gw15xkJFfPqAecCZLsGETawtv Ganesh@LAPTOP-034EJ4CI
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
 }
